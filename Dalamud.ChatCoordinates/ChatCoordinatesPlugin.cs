25a4b4ee3d2bdcc5eba483fd08e8cd6d2aaab08a
Kissor Jeyabalan <kissor@fluxloop.com>
2020-07-24 14:06:36 +0200

    Bypass MapLinkPayload provided by Dalamud framework

--- Dalamud.ChatCoordinates/ChatCoordinatesPlugin.cs
+++ Dalamud.ChatCoordinates/ChatCoordinatesPlugin.cs
@@ -1,15 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
+using Dalamud.Game;
 using Dalamud.Game.Chat;
 using Dalamud.Game.Chat.SeStringHandling;
 using Dalamud.Game.Chat.SeStringHandling.Payloads;
 using Dalamud.Game.Command;
-using Dalamud.Game.Internal.Gui;
 using Dalamud.Plugin;
 using Lumina.Excel.GeneratedSheets;
-
 
 namespace ChatCoordinates
 {
@@ -18,9 +18,26 @@ namespace ChatCoordinates
         public string Name => "ChatCoordinates Plugin";
         private DalamudPluginInterface _pi;
         
+        private GetUIObjectDelegate getUIObject;
+        private GetUIMapObjectDelegate getUIMapObject;
+        private OpenMapWithFlagDelegate openMapWithFlag;
+        
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate IntPtr GetUIObjectDelegate();
+        [UnmanagedFunctionPointer(CallingConvention.ThisCall)]
+        private delegate IntPtr GetUIMapObjectDelegate(IntPtr UIObject);
+        [UnmanagedFunctionPointer(CallingConvention.ThisCall, CharSet = CharSet.Ansi)]
+        private delegate bool OpenMapWithFlagDelegate(IntPtr UIMapObject, string flag);
+        
+        
+        
         public void Initialize(DalamudPluginInterface pluginInterface)
         {
             _pi = pluginInterface;
+
+            var targetModule = Process.GetCurrentProcess().MainModule;
+            var sig = new SigScanner(targetModule, true);
+            getUIObject = Marshal.GetDelegateForFunctionPointer<GetUIObjectDelegate>(sig.ScanText("E8 ?? ?? ?? ?? 48 8B C8 48 8B 10 FF 52 40 80 88 ?? ?? ?? ?? 01 E9"));
 
             _pi.CommandManager.AddHandler("/coord", new CommandInfo(CommandHandler)
             {
@@ -39,12 +56,15 @@ namespace ChatCoordinates
         public void PrintTerritory(string command, string args)
         {
             var unsignedTerritoryType = Convert.ToUInt32(_pi.ClientState.TerritoryType);
-
             var territorySheet = _pi.Data.GetExcelSheet<TerritoryType>().GetRow(_pi.ClientState.TerritoryType);
-              _pi.Framework.Gui.Chat.Print($"------ {territorySheet.Name} ------");
-              _pi.Framework.Gui.Chat.Print($"Map: {territorySheet.Map.ToString()}");
-              _pi.Framework.Gui.Chat.Print($"Territory: {unsignedTerritoryType}");
+            var map = _pi.Data.GetExcelSheet<Map>().GetRow(territorySheet.Map.Value.RowId);
+            _pi.Framework.Gui.Chat.Print(
+                $"------ {territorySheet.Name} - {territorySheet.Map.Value.PlaceName.Value.Name} ------");
+            _pi.Framework.Gui.Chat.Print($"Map: {territorySheet.Map.Value.RowId}");
+            _pi.Framework.Gui.Chat.Print($"Territory: {unsignedTerritoryType}");
+            _pi.Framework.Gui.Chat.Print("---------");
         }
+
         public void CommandHandler(string command, string arguments)
         {
             var arg = arguments.Trim().Replace("\"", "");
@@ -77,34 +97,15 @@ namespace ChatCoordinates
 
             if (!xSet || !ySet)
                 ShowHelp(command);
-            
+
+
             var unsignedTerritoryType = Convert.ToUInt32(_pi.ClientState.TerritoryType);
             var territorySheet = _pi.Data.GetExcelSheet<TerritoryType>().GetRow(_pi.ClientState.TerritoryType);
-            var mapLink = new MapLinkPayload(unsignedTerritoryType, territorySheet.Map, x, y);
-            _pi.Framework.Gui.OpenMapWithMapLink(mapLink);
             
-            var nameString = $"{mapLink.PlaceName} {mapLink.CoordinateString}";
-            var payloads = new List<Payload>(new Payload[]
-            {
-                mapLink,
-                new TextPayload(nameString),
-                RawPayload.LinkTerminator,
-            });
-
-            var textArrowPayload = new List<Payload>(new Payload[]
-            {
-                new UIForegroundPayload(0x01F4),
-                new UIGlowPayload(0x01F5),
-                new TextPayload($"{(char)SeIconChar.LinkMarker}"),
-                UIGlowPayload.UIGlowOff,
-                UIForegroundPayload.UIForegroundOff, 
-            });
-            payloads.InsertRange(1, textArrowPayload);
-            var squareEnixString = new SeString(payloads);
-            
+            OpenMapWithFlag(unsignedTerritoryType, territorySheet.Map.Value.RowId, territorySheet.Map.Value.SizeFactor, x, y);
             _pi.Framework.Gui.Chat.PrintChat(new XivChatEntry
             {
-                MessageBytes = squareEnixString.Encode()
+                MessageBytes = _pi.SeStringManager.CreateMapLink(territorySheet.Map.Value.PlaceName.Value.Name, x, y).Encode()
             });
         }
 
@@ -118,11 +119,37 @@ namespace ChatCoordinates
                 $"{command} 8.8 11.5";
             _pi.Framework.Gui.Chat.Print(helpText);
         }
-        
+
         public void Dispose()
         {
             _pi.CommandManager.RemoveHandler("/coords");
             _pi.Dispose();
+        }
+        
+        private void OpenMapWithFlag(uint territoryType, uint mapId, ushort sizeFactor, float niceX, float niceY, float fudge = 0.05f)
+        {
+            var rawX = ConvertMapCoordinateToRawPosition(niceX + fudge, sizeFactor);
+            var rawY = ConvertMapCoordinateToRawPosition(niceY + fudge, sizeFactor);
+
+            var num1 = getUIObject();
+            getUIMapObject = _pi.Framework.Address.GetVirtualFunction<GetUIMapObjectDelegate>(num1, 0, 8);
+            var num2 = getUIMapObject(num1);
+            openMapWithFlag = _pi.Framework.Address.GetVirtualFunction<OpenMapWithFlagDelegate>(num2, 0, 63);
+            var dataString =
+                $"m:{(object) territoryType},{(object) mapId},{(object) rawX},{(object) rawY}";
+            openMapWithFlag(num2, dataString);
+        }
+
+        private string GetCoordinateString(float x, float y)
+        {
+            return "( " + (Math.Truncate(((double) x + 0.0199999995529652) * 10.0) / 10.0).ToString("0.0") + "  , " +
+                   (Math.Truncate(((double) y + 0.0199999995529652) * 10.0) / 10.0).ToString("0.0") + " )";
+        }
+        
+        private int ConvertMapCoordinateToRawPosition(float pos, float scale)
+        {
+            float num = scale / 100f;
+            return (int) ((float) ((pos - 1.0) * num / 41.0 * 2048.0 - 1024.0) / num * 1000f);
         }
     }
 }

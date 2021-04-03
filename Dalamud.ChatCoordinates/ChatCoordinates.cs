f1d4c68cf164caa41cce28ab5c5b1e2cb05ebbfc
Kissor Jeyabalan <git@kij.no>
2021-04-03 20:24:16 +0200

    Added fudging to edge case coordinate for all zones & refactored

---
+++ Dalamud.ChatCoordinates/ChatCoordinates.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Data.SqlClient;
+using ChatCoordinates.Extensions;
+using ChatCoordinates.Functions;
+using ChatCoordinates.Managers;
+using ChatCoordinates.Models;
+using Dalamud.Game.Command;
+using Dalamud.Plugin;
+
+namespace ChatCoordinates
+{
+    public class ChatCoordinates : IDalamudPlugin
+
+    {
+        private bool _disposed;
+        public string Name => nameof(ChatCoordinates);
+
+        public DalamudPluginInterface Interface { get; private set; } = null!;
+        public CoordinateFunctions CoordinateFunctions { get; private set; } = null!;
+        public AetheryteFunctions AetheryteFunctions { get; private set; } = null!;
+
+        private Lazy<TerritoryManager> _territoryManager;
+        public TerritoryManager TerritoryManager => _territoryManager.Value;
+
+        private Lazy<AetheryteManager> _aetheryteManager;
+        public AetheryteManager AetheryteManager => _aetheryteManager.Value;
+
+        public void Initialize(DalamudPluginInterface pluginInterface)
+        {
+            Interface = pluginInterface ??
+                        throw new ArgumentNullException(nameof(pluginInterface), "Dalamud interface cannot be null");
+            CoordinateFunctions = new CoordinateFunctions(this);
+            AetheryteFunctions = new AetheryteFunctions(this);
+
+            _territoryManager = new Lazy<TerritoryManager>(() => new TerritoryManager(this));
+            _aetheryteManager = new Lazy<AetheryteManager>(() => new AetheryteManager(this));
+            
+
+            Interface.CommandManager.AddHandler("/coord", new CommandInfo(this.OnCoordinateCommand)
+            {
+                HelpMessage = "/coord <x> <y> [: <partial zone name>] -- Places map marker at given coordinates"
+            });
+
+            Interface.CommandManager.AddHandler("/ctp", new CommandInfo(this.OnCoordinateTeleportCommand)
+            {
+                HelpMessage =
+                    "/ctp <x> <y> [: <partial zone name>] -- Places map marker and teleports to closest aetheryte"
+            });
+
+            Interface.CommandManager.AddHandler("/ctpt", new CommandInfo(this.OnCoordinateTeleportCommand)
+            {
+                HelpMessage =
+                    "/ctpt <x> <y> [: <partial zone name>] -- Places map marker and teleports to closets aetheryte with ticket"
+            });
+        }
+
+        private void OnCoordinateCommand(string cmd, string args)
+        {
+            ProcessCoordinate(cmd, args);
+        }
+
+        private void OnCoordinateTeleportCommand(string cmd, string args)
+        {
+            var coordinate = ProcessCoordinate(cmd, args);
+            if (coordinate == null) return;
+            
+            coordinate.Teleport = true;
+            
+            if (cmd.ToLower().Equals("/ctpt"))
+            {
+                coordinate.UseTicket = true;
+            }
+            
+            AetheryteFunctions.Teleport(coordinate);
+        }
+
+        private Coordinate? ProcessCoordinate(string cmd, string args)
+        {
+            if (Interface.ClientState.TerritoryType == 0)
+            {
+                Interface.Framework.Gui.Chat.Print("Unable to get territory info. Please switch zone to initialize plugin.");
+                return null;
+            }
+            
+            if (string.IsNullOrWhiteSpace(args) || args.Equals("help", StringComparison.OrdinalIgnoreCase))
+            {
+                ShowHelp(cmd);
+                return null;
+            }
+            
+            var coordinate = args.ParseCoordinate(this);
+            CoordinateFunctions.PlaceMarker(coordinate);
+            
+            return coordinate;
+        }
+        
+        private void ShowHelp(string cmd)
+        {
+            switch (cmd)
+            {
+                case "/coord":
+                    Interface.Framework.Gui.Chat.Print("Places a map marker at given coordinates. Colon (:) can be used a delimiter to place marker at given zone. Placed marker can be shared by typing <flag>.");
+                    Interface.Framework.Gui.Chat.Print("/coord <x> <y> [: <zone>]");
+                    Interface.Framework.Gui.Chat.Print("/coord 8.8 11.5");
+                    Interface.Framework.Gui.Chat.Print("/coord 8.8,11.5");
+                    Interface.Framework.Gui.Chat.Print("/coord X: 10.7 Y: 11.7 : Lakeland");
+                    Interface.Framework.Gui.Chat.Print("/coord 10.7 11.7 : Lakeland");
+                    break;
+                case "/ctp":
+                    Interface.Framework.Gui.Chat.Print("Places a map marker at given coordinate and teleports to the closest aetheryte. Colon (:) is used as delimiter for zone.");
+                    Interface.Framework.Gui.Chat.Print("/ctp 10.7 11.7 : Lakeland");
+                    Interface.Framework.Gui.Chat.Print("/ctp 10.7 11.7 : Lakeland");
+                    Interface.Framework.Gui.Chat.Print("/ctp X: 10.7 Y: 11.7 : Lakeland");
+                    break;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) return;
+            if (disposing)
+            {
+                CoordinateFunctions.Dispose();
+
+                Interface.CommandManager.RemoveHandler("/coord");
+                Interface.CommandManager.RemoveHandler("/ctp");
+                Interface.CommandManager.RemoveHandler("/ctpt");
+                Interface.Dispose();
+            }
+
+            _disposed = true;
+        }
+    }
+}
---
+++ Dalamud.ChatCoordinates/Functions/AetheryteFunctions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ChatCoordinates.Models;
+
+namespace ChatCoordinates.Functions
+{
+    public class AetheryteFunctions
+    {
+        private ChatCoordinates _plugin;
+
+        public AetheryteFunctions(ChatCoordinates plugin)
+        {
+            _plugin = plugin ?? throw new ArgumentNullException(nameof(plugin), "ChatCoordinates cannot be null");
+        }
+
+        public void Teleport(Coordinate coordinate)
+        {
+            if (!coordinate.Teleport) return;
+
+            var tpCmd = _plugin.Interface.CommandManager.Commands.ContainsKey("/tp")
+                ? _plugin.Interface.CommandManager.Commands["/tp"]
+                : null;
+
+            var tpTicketCmd = _plugin.Interface.CommandManager.Commands.ContainsKey("/tpt")
+                ? _plugin.Interface.CommandManager.Commands["/tpt"]
+                : null;
+
+            if (tpCmd != null && tpTicketCmd != null)
+            {
+                var aetheryte = _plugin.AetheryteManager.GetClosestAetheryte(coordinate);
+                if (aetheryte != null)
+                {
+                    if (coordinate.UseTicket)
+                    {
+                        tpTicketCmd.Handler.Invoke("/tpt", aetheryte.Name);
+                    }
+                    else
+                    {
+                        tpCmd.Handler.Invoke("/tp", aetheryte.Name);
+                    }
+                }
+                else
+                {
+                    _plugin.Interface.Framework.Gui.Chat.Print("Failed to find closest aetheryte.");
+                }
+            }
+            else
+            {
+                _plugin.Interface.Framework.Gui.Chat.PrintError("Teleporting requires Teleporter plugin to be installed.");
+            }
+        }
+    }
+}
